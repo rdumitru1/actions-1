ced1d21c5e37dffd96f0c1daaaeafafb29cb0990
dmr <dumitru.rm@gmail.com>
2025-06-02 22:53:20 +0300

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
     // 1) Get some input values
@@ -9,10 +9,11 @@ function run() {
     const distFolder = core.getInput('dist-folder', { required:true });
 
     // 2) Upload Files
-    const s3Uri = `s3://${bucket}`
+    const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom JavaScript Action!')
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 
 run();
